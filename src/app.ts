7db44aa09f8d6a0f9e3230fc3db75431cc73862f
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-13 13:01:31 -0300

    Bug found inside the file package.json

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -12,6 +14,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
